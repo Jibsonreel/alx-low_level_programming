06837a796846f95b2a9a49b4f0f4a9ab5e02d0f9
Jibsonreel <110991534+Jibsonreel@users.noreply.github.com>
2022-09-09 09:10:03 -0700

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+* main - Application entry function
+* Description: use printf function
+* Return: 0
+*/
+
+int main(void)
+{
+        printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+        return (0);        
+}        
