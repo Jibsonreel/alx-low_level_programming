2c71b42c60cc956a0c38aa9731e23f19feecea94
Jibsonreel <110991534+Jibsonreel@users.noreply.github.com>
2022-09-09 09:30:04 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
-* main - entry point
-Description: prints a message
-* Return: 0
-*/
+ * main - Entry point
+ * Description: prints a message
+ * Return: 0
+ */
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
