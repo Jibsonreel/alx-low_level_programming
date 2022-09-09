63d5382ba60a5721c6499b99c2c944346de1d0c5
Jibsonreel <jyqualitypaint@gmail.com>
2022-09-09 08:55:16 -0700

    my fifth c p

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+* main - entry point
+Description: prints a message
+* Return: 0
+*/
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}	
