c4c59f33b3846d0ab9fd497f8bf4b9555f8d0897
Jibsonreel <jyqualitypaint@gmail.com>
2022-09-12 06:56:34 -0700

    my first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- * Description: prints the alphabet in lowercase\n
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	int letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		putchar(letter);
-	}
-	putchar('\n');
-	return (0);
-}
